0c890da1e7aeb80ddcee2d5ec1ee3c5a771362f8
Thomas J Kane <thomasjkane@optonline.net>
2019-04-03 21:41:39 -0400

    commit

--- practice.js
+++ practice.js
@@ -1,21 +1,21 @@
-# var name = "Dylan"; 
-let firstName = "Dylan"; # let replaces var
-const lastName = "Israel"; # Constants cant be changed
+// var name = "Dylan";
+let firstName = "Dylan"; // let replaces var
+const lastName = "Israel"; // Constants cant be changed
 
 
 
 
-console.log(`$(firstName) $(lastName)`) # Concatenation
+console.log(`$(firstName) $(lastName)`) // Concatenation
 
 console.log(`
-	$(firstName) $(lastName)    `.trim().lenght) # trim removed the empty spaces where lenght is the lenght of the string
+	$(firstName) $(lastName)    `.trim().lenght) // trim removed the empty spaces where lenght is the lenght of the string
 
 
-console.log(`$(firstName) $(lastName)`.toUpperCase()) # Change everything to upper case
+console.log(`$(firstName) $(lastName)`.toUpperCase()) // Change everything to upper case
 
-console.log(`$(firstName) $(lastName)`.toLowerCase()) # Change everthing to lower case
+console.log(`$(firstName) $(lastName)`.toLowerCase()) // Change everthing to lower case
 
-console.log(`$(firstName) $(lastName)`.split(' ')) # Break a sting into an array at the spaces
+console.log(`$(firstName) $(lastName)`.split(' ')) // Break a sting into an array at the spaces
 
 const fullName = `$(FirstName) $(lastName)`;
 
@@ -25,23 +25,23 @@ let example = 7;
 
 console.log(example);
 
-console.log(parseInt(example); # Parses to only the integer
+console.log(parseInt(example); // Parses to only the integer
 
-console.log(parseFloat(example); # Parses to only the floating point number
+console.log(parseFloat(example); // Parses to only the floating point number
 
-console.log(example.toFixed(2)); # add/set decimal places
+console.log(example.toFixed(2)); // add/set decimal places
 
-console.log(typeof example); # Will show the type of object "sting" "number"
+console.log(typeof example); // Will show the type of object "sting" "number"
 
-let example1 = false; # Booleans are true of false
+let example1 = false; // Booleans are true of false
 let example2 = true;
 
-console.log(Boolean(example1)); # will print false
-console.log(Boolean(example2)); # will print true
+console.log(Boolean(example1)); // will print false
+console.log(Boolean(example2)); // will print true
 
-// null would be a false value, 
-// undefined would be false, 
-// Nan (not a number) false, 
+// null would be a false value,
+// undefined would be false,
+// Nan (not a number) false,
 // strings are false but a space is true
 // number values are true, except 0 which is false
 
@@ -50,15 +50,15 @@ let array1 = [5, 5, 6];
 console.log(array1.lenght);
 console.log(array1[0]);
 
-array1.push(8); # appaend an 8 to the array
-array1.push(8, 9, 10); # appaend an array
-array1.pop(); # remove the last value
-array1[0] = 1; # over writ the first vaule in the array with 1
+array1.push(8); // appaend an 8 to the array
+array1.push(8, 9, 10); // appaend an array
+array1.pop(); // remove the last value
+array1[0] = 1; // over writ the first vaule in the array with 1
 
 array1.forEach((element) => {
 
 	console.log(element)
-}); # loop each element in the array
+}); // loop each element in the array
 
 // Objects with key value pairs
 let object1 = {
@@ -72,36 +72,12 @@ let object1 = {
 	cats: ['Milo', 'Tito', 'Achilies']
 };
 
-object1.age = 31; # Reset the target age 
+object1.age = 31; // Reset the target age
 
 console.log(oject1);
 console.log(object1.lastName);
 console.log(opject1.address.city);
-console.log(Object.keys(object1)); # only print the keys
-console.log(Object.values(object1)); # only print the values
-console.log(oject1.hasOwnPropperty('firstName')); # will return true
-console.log(oject1.hasOwnPropperty('firstName2')); # will return false
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(Object.keys(object1)); // only print the keys
+console.log(Object.values(object1)); // only print the values
+console.log(oject1.hasOwnPropperty('firstName')); // will return true
+console.log(oject1.hasOwnPropperty('firstName2')); // will return false
